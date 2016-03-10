43d2586cbe7fa34fe08625608aa34f2216eeea6a
Samuel Moritz <samuel.moritz@gmail.com>
2016-03-10 19:56:55 +0100

    More porting to Rust. Still not working.

--- picirq.rs
+++ picirq.rs
@@ -1,9 +1,88 @@
-#[no_mangle]
-pub extern fn picenable(irq: u16) -> () {
-    //TODO: Impl
+#![feature(asm)]
+
+mod x86;
+mod traps;
+
+use traps::T_IRQ0;
+
+// I/O Addresses of the two programmable interrupt controllers
+const IO_PIC1:u16 = 0x20;    // Master (IRQs 0-7)
+const IO_PIC2:u16 = 0xA0;    // Slave (IRQs 8-15)
+
+const IRQ_SLAVE:u8 = 2;       // IRQ at which slave connects to master
+
+// Current IRQ mask.
+// Initial IRQ mask has interrupt 2 enabled (for slave 8259A).
+static mut irqmask:u16 = 0xFFFF & !(1<<IRQ_SLAVE);
+
+fn picsetmask(mask:u16) {
+  unsafe {
+    irqmask = mask;
+  }
+  x86::out8(IO_PIC1+1, (mask & 0xff) as u8);
+  x86::out8(IO_PIC2+1, ((mask >> 8) & 0xff) as u8);
 }
 
 #[no_mangle]
+pub extern fn picenable(irq: u16) -> () {
+    unsafe {
+        picsetmask(irqmask & !(1<<irq));
+    }
+}
+
+// Initialize the 8259A interrupt controllers.
+#[no_mangle]
 pub extern fn picinit() -> () {
-    //TODO: Impl    
+    // mask all interrupts
+    x86::out8(IO_PIC1+1, 0xFF);
+    x86::out8(IO_PIC2+1, 0xFF);
+
+    // Set up master (8259A-1)
+
+    // ICW1:  0001g0hi
+    //    g:  0 = edge triggering, 1 = level triggering
+    //    h:  0 = cascaded PICs, 1 = master only
+    //    i:  0 = no ICW4, 1 = ICW4 required
+    x86::out8(IO_PIC1, 0x11);
+
+    // ICW2:  Vector offset
+    x86::out8(IO_PIC1+1, T_IRQ0);
+
+    // ICW3:  (master PIC) bit mask of IR lines connected to slaves
+    //        (slave PIC) 3-bit # of slave's connection to master
+    x86::out8(IO_PIC1+1, 1<<IRQ_SLAVE);
+
+    // ICW4:  000nbmap
+    //    n:  1 = special fully nested mode
+    //    b:  1 = buffered mode
+    //    m:  0 = slave PIC, 1 = master PIC
+    //      (ignored when b is 0, as the master/slave role
+    //      can be hardwired).
+    //    a:  1 = Automatic EOI mode
+    //    p:  0 = MCS-80/85 mode, 1 = intel x86 mode
+    x86::out8(IO_PIC1+1, 0x3);
+
+    // Set up slave (8259A-2)
+    x86::out8(IO_PIC2, 0x11);                  // ICW1
+    x86::out8(IO_PIC2+1, T_IRQ0 + 8);      // ICW2
+    x86::out8(IO_PIC2+1, IRQ_SLAVE);           // ICW3
+    // NB Automatic EOI mode doesn't tend to work on the slave.
+    // Linux source code says it's "to be investigated".
+    x86::out8(IO_PIC2+1, 0x3);                 // ICW4
+
+    // OCW3:  0ef01prs
+    //   ef:  0x = NOP, 10 = clear specific mask, 11 = set specific mask
+    //    p:  0 = no polling, 1 = polling mode
+    //   rs:  0x = NOP, 10 = read IRR, 11 = read ISR
+    x86::out8(IO_PIC1, 0x68);             // clear specific mask
+    x86::out8(IO_PIC1, 0x0a);             // read IRR by default
+
+    x86::out8(IO_PIC2, 0x68);             // OCW3
+    x86::out8(IO_PIC2, 0x0a);             // OCW3
+
+    unsafe {
+        if(irqmask != 0xFFFF) {
+          picsetmask(irqmask);
+        }
+    }
 }
--- traps.rs
+++ traps.rs
@@ -1,1 +1,3 @@
+pub const T_IRQ0:u8 = 32;      // IRQ 0 corresponds to int T_IRQ
+
 pub const IRQ_TIMER:u16 = 0;
